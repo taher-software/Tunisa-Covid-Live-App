70cc2322ed4b5eb266f6e854e97e0df036d2eb4e
Taher <ttaherhagui@gmail.com>
2021-12-22 07:04:42 +0100

    add test for home page component.

--- src/__test__/components.test.js
+++ src/__test__/components.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import NavBar from '../Navbar';
+import HomePage from '../components/HomePage/home';
 
+const mockStore = configureStore([]);
 test('render correctly navbar', () => {
   const tree = renderer.create(
     <Router>
@@ -10,4 +14,26 @@ test('render correctly navbar', () => {
     </Router>,
   ).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+describe('render correctly Homepage component', () => {
+  let store;
+  let tree;
+  beforeEach(() => {
+    store = mockStore({
+      latest: {},
+      dayBefore: {},
+    });
+    store.dispatch = jest.fn();
+    tree = renderer.create(
+      <Provider store={store}>
+        <Router>
+          <HomePage />
+        </Router>
+      </Provider>,
+    );
+  });
+  it('should render with given state from redux store', () => {
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 });
